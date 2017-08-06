b7e2b09eef5e4d00e09bb6be8a536afd51998093
gowrimanoharir <gowrimanohari.r@gmail.com>
2017-08-06 15:24:15 -0500

    added html routes

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+//var path=require('path');
 
 var app = express();
 
 
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3010;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,8 +13,12 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 
-require('./app/routing/apiRoutes.js')(app);
+//require('./app/routing/apiRoutes.js')(app);
 require('./app/routing/htmlRoutes.js')(app);
+
+  /*  app.use(function(req, res){
+        res.sendFile(path.join(__dirname, 'app/public/home.html'));
+    });*/
 
 app.listen(PORT, function(){
     console.log(`Listening on PORT ${PORT}`);
